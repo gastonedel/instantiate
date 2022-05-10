8b2de46eb47c9c7efaa89fecc7dac21ad246b1ba
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-10 10:00:21 -0300

    Initial commit

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ObjectClone;
+
+    public void CloneObject()
+    {
+        
+            Instantiate(ObjectClone);
+            Instantiate(ObjectClone);
+            Instantiate(ObjectClone);
+        
+
+    }
+}
