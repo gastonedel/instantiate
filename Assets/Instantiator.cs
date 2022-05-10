cbfaf859dff6a99f504047d2d47207bf84e99ba6
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-10 10:41:53 -0300

    while

--- Assets/Instantiator.cs
+++ Assets/Instantiator.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Instantiator : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject ObjectClone;
-
+    public int cloneAmount;
+    public Text txtCloneAmount;
     public void CloneObject()
-    {
         
+    {
+        cloneAmount = int.Parse(txtCloneAmount.text);
+        int i = 0;
+        while (i<cloneAmount)
+        {
             Instantiate(ObjectClone);
-            Instantiate(ObjectClone);
-            Instantiate(ObjectClone);
+            i++;
+        }
+            
+
         
 
     }
---
+++ Assets/imputfield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imputfield : MonoBehaviour
+{
+    public InputField ifUsername;
+    public Text txtUsername;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Usergreetings()
+    {
+        Debug.Log(int.Parse (txtUsername.text) * 2 );
+    }
+}
